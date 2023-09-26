08b8ea72cb7beebd7929dd45ea1fe59b80cca0ab
daria-nutcracker <dsshchelokova.42@gmail.com>
2023-09-26 16:38:55 +0300

    create Runnable

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         MyThread myThread = new MyThread();
         myThread.start();
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread1 = new Thread(myRunnable);
+        thread1.start();
 
     }
 }
