ad4078516d0d5c7d23fa37395bd4267f8fa7a68a
daria-nutcracker <dsshchelokova.42@gmail.com>
2023-09-26 20:27:52 +0300

    изучение synchronized для статических переменных 2

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,20 @@ import static java.lang.Thread.sleep;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Resource resource = new Resource(0);
-       MyThread myThread1 = new MyThread();
-       myThread1.setName("one");
-       MyThread myThread2 = new MyThread();
-       myThread1.setResource(resource);
-       myThread2.setResource(resource);
-       myThread1.start();
-      // myThread1.join();
-       myThread2.start();
+
+        MyThreadForStaticPar myThreadForStaticPar1 = new MyThreadForStaticPar ();
+        MyThreadForStaticPar myThreadForStaticPar2 = new MyThreadForStaticPar ();
+        myThreadForStaticPar1.start();
+        myThreadForStaticPar2.start();
+//        Resource resource = new Resource(0);
+//       MyThread myThread1 = new MyThread();
+//       MyThread myThread2 = new MyThread();
+//       myThread1.setResource(resource);
+//       myThread2.setResource(resource);
+//       myThread1.start();
+//       myThread2.start();
 
        System.out.println(currentThread().getName());
-        System.out.println(resource.getI());
-//        MyRunnable myRunnable = new MyRunnable();
-//        Thread thread1 = new Thread(myRunnable);
-//        thread1.start();
-
+//        System.out.println(resource.getI());
     }
 }
--- src/MyThreadForStaticPar.java
+++ src/MyThreadForStaticPar.java
@@ -5,7 +5,8 @@ public class MyThreadForStaticPar extends Thread{
     public void run () {
 
         for (int i = 0; i < 5; i++) {
-            System.out.println("Current thread " + Thread.currentThread().getName() + "  i: " + ResourceWithStaticParameter.increaseI(););
+            System.out.println("Current thread " + Thread.currentThread().getName() +
+                    "  i: " + ResourceWithStaticParameter.increaseI());
         }
     }
 }
